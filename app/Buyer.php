1129b1193a7df7bddb824985995f2393ebc70a0b
Mauricio Flores Hernandez <mau1361317@gmail.com>
2020-02-05 21:29:48 -0600

    Scopes para usar la inyeccion del modelo

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,8 +2,14 @@
 
 namespace App;
 
+use App\Scopes\BuyerScope;
+
 class Buyer extends User
 {
+    public static function boot(){
+        parent::boot();
+        static::addGlobalScope(new BuyerScope);
+    }
     public function transactions(){
         return $this->hasMany(Transaction::class);
     }
