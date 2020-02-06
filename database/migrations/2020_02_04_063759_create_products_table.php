ab2da73145889e29c31daa4a58b689f34664bec3
Mauricio Flores Hernandez <mau1361317@gmail.com>
2020-02-05 21:34:00 -0600

    Soft delete en models

--- database/migrations/2020_02_04_063759_create_products_table.php
+++ database/migrations/2020_02_04_063759_create_products_table.php
@@ -22,6 +22,7 @@ class CreateProductsTable extends Migration
             $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
             $table->string('image');
             $table->integer('seller_id')->unsigned();
+            $table->softDeletes();
             $table->timestamps();
             
             $table->foreign('seller_id')->references('id')->on('users');
